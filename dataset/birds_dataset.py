f9d8eea32fd9818d845a7ceb5cdd449807fe4a10
Robin Dong <robin.k.dong@gmail.com>
2019-05-18 04:20:32 +0000

    Some subdirectory may have name that contains two '.'(dot)

--- dataset/birds_dataset.py
+++ dataset/birds_dataset.py
@@ -6,13 +6,17 @@ import torch.utils.data as data
 
 class BirdsDataset(data.Dataset):
     """ All images and classes for birds through the world """
-    def __init__(self, root_path):
+    def __init__(self, root_path, num_classes):
         self.image_list = []
         self.labelmap = {}
         for directory in os.walk(root_path):
             for dir_name in directory[1]: # All subdirectories
-                type_id, type_name = dir_name.split('.')
-                type_id = int(type_id)
+                pos = dir_name.find('.')
+                type_id = int(dir_name[0:pos])
+                type_name = dir_name[pos+1:]
+                if type_id < 0 or type_id > num_classes:
+                    print('Wrong directory: {}!'.format(dir_name))
+                    continue
                 self.labelmap[type_id] = type_name
                 for image_file in os.listdir(os.path.join(root_path, dir_name)):
                     full_path = os.path.join(root_path, dir_name, image_file)
