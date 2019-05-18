a053a38389094726a015a0c83f0d9b40197a6be2
Robin Dong <robin.k.dong@gmail.com>
2019-05-18 04:21:11 +0000

    Add parser and cfg for hyperparameters. And get 'accuracy' for metrics

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import time
+import argparse
 
 import torch
 import torch.optim as optim
@@ -9,16 +10,27 @@ from torch.autograd import Variable
 from dataset.birds_dataset import BirdsDataset
 from nets import resnet
 
-def train():
-    dataset = BirdsDataset('/disk3/donghao/data/bird/')
-    data_loader = data.DataLoader(dataset, 32, num_workers=8, shuffle=True, pin_memory=True)
+cfg = {
+    'nr_images': 3934740,
+    'num_classes': 11000,
+    'num_workers': 4,
+    'verbose_steps': 100,
+    }
 
-    net = resnet.resnet18(num_classes=10900).cuda()
+def train(args):
+    t0 = time.time()
+    dataset = BirdsDataset(args.dataset_root, cfg['num_classes'])
+    t1 = time.time()
+    print('Load dataset with {} secs'.format(t1 - t0))
+    data_loader = data.DataLoader(dataset, args.batch_size, num_workers=cfg['num_workers'],
+                                  shuffle=True, pin_memory=True)
 
-    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
+    net = resnet.resnet18(num_classes=cfg['num_classes']).cuda()
+
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
 
     batch_iterator = iter(data_loader)
-    for iteration in range(1000):
+    for iteration in range(args.max_epoch * cfg['nr_images'] // args.batch_size):
         try:
             images, type_ids = next(batch_iterator)
         except StopIteration:
@@ -46,8 +58,20 @@ def train():
         optimizer.step()
         t1 = time.time()
 
-        if iteration % 10 == 0:
-            print('loss: %.4f || timer: %.4f sec.' % (loss.item(), t1 - t0))
+        if iteration % cfg['verbose_steps'] == 0:
+            # accuracy
+            _, predict = torch.max(out, 1)
+            correct = (predict == type_ids)
+            accuracy = correct.sum().item() / correct.size()[0]
+            print('loss: %.4f | accuracy: %.4f | timer: %.4f sec.' % (loss.item(), accuracy, (t1 - t0)))
 
 if __name__ == '__main__':
-    train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default=32, type=int, help='Batch size for training')
+    parser.add_argument('--max_epoch', default=100, type=int, help='Maximum epoches for training')
+    parser.add_argument('--dataset_root', default='/media/data2/i18n/V1', type=str, help='Root path of data')
+    parser.add_argument('--lr', default=0.1, type=float, help='Initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, help='Momentum value for optimizer')
+    args = parser.parse_args()
+
+    train(args)
