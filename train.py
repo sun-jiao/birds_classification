e3c8aef0be77659a1bd8b84823de3484c3e2e387
Robin Dong <robin.k.dong@gmail.com>
2019-05-18 13:25:37 +0000

    Save and resume checkpoint so we can finetuning model

--- train.py
+++ train.py
@@ -14,8 +14,14 @@ cfg = {
     'nr_images': 3934740,
     'num_classes': 11000,
     'num_workers': 4,
-    'verbose_steps': 100,
+    'verbose_period': 100,
+    'save_period': 20000,
+    'save_folder': 'ckpt/',
+    'ckpt_name': 'bird_cls',
     }
+
+def save_ckpt(net, iteration):
+    torch.save(net.state_dict(), cfg['save_folder'] + cfg['ckpt_name'] + '_' + str(iteration) + '.pth')
 
 def train(args):
     t0 = time.time()
@@ -25,12 +31,16 @@ def train(args):
     data_loader = data.DataLoader(dataset, args.batch_size, num_workers=cfg['num_workers'],
                                   shuffle=True, pin_memory=True)
 
-    net = resnet.resnet18(num_classes=cfg['num_classes']).cuda()
+    net = resnet.resnext50_32x4d(num_classes=cfg['num_classes']).cuda()
+    if args.resume:
+        print('Resuming training, loading {}...'.format(args.resume))
+        net.load_weights(args.resume)
 
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
 
     batch_iterator = iter(data_loader)
     for iteration in range(args.max_epoch * cfg['nr_images'] // args.batch_size):
+        t0 = time.time()
         try:
             images, type_ids = next(batch_iterator)
         except StopIteration:
@@ -42,12 +52,7 @@ def train(args):
         images = Variable(images.cuda())
         type_ids = Variable(type_ids.cuda())
 
-        ids_one_hot = torch.zeros(len(type_ids), type_ids.max() + 1, device='cuda')
-        ids_one_hot = ids_one_hot.scatter_(1, type_ids.unsqueeze(1), 1.)
-        #print('ids_one_hot', ids_one_hot, ids_one_hot.shape)
-
         # forward
-        t0 = time.time()
         out = net(images.permute(0, 3, 1, 2).float())
         #print('out:', out, out.shape)
 
@@ -58,12 +63,21 @@ def train(args):
         optimizer.step()
         t1 = time.time()
 
-        if iteration % cfg['verbose_steps'] == 0:
+        if iteration % cfg['verbose_period'] == 0:
             # accuracy
             _, predict = torch.max(out, 1)
             correct = (predict == type_ids)
             accuracy = correct.sum().item() / correct.size()[0]
-            print('loss: %.4f | accuracy: %.4f | timer: %.4f sec.' % (loss.item(), accuracy, (t1 - t0)))
+            print('step: %d loss: %.4f | accuracy: %.4f | time: %.4f sec.' %
+                  (iteration, loss.item(), accuracy, (t1 - t0)))
+
+        if iteration % cfg['save_period'] == 0:
+            # save checkpoint
+            print('Saving state, iter:', iteration)
+            save_ckpt(net, iteration)
+
+    # final checkpoint
+    save_ckpt(net, iteration)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -72,6 +86,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_root', default='/media/data2/i18n/V1', type=str, help='Root path of data')
     parser.add_argument('--lr', default=0.1, type=float, help='Initial learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, help='Momentum value for optimizer')
+    parser.add_argument('--resume', default=None, type=str, help='Checkpoint file to resume training from')
     args = parser.parse_args()
 
     train(args)
