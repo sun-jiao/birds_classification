b8c642fdc7cd53b9adef41cbd39a612e8d3d6f19
Robin Dong <robin.k.dong@gmail.com>
2019-09-02 23:37:35 -0400

    Alleviate some augmentations for better accuracy

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -3,22 +3,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+IMAGE_SHAPE = (100, 100)
+
 
 class Augmentations(nn.Module):
 
     def __init__(self):
         super(Augmentations, self).__init__()
         self.gaussian_blur = nn.Conv2d(3, 3, (3, 3), padding=1, groups=3, bias=False).cuda()
-        self.heavy_blur = nn.Conv2d(3, 3, (5, 5), padding=2, groups=3, bias=False).cuda()
-        self.dropout = nn.Dropout(p=0.1).cuda()
+        '''self.heavy_blur = nn.Conv2d(3, 3, (5, 5), padding=2, groups=3, bias=False).cuda()
+        self.dropout = nn.Dropout(p=0.1).cuda()'''
 
     def forward(self, image):
+        # Gaussian blur
         self.gaussian_blur.weight.data.normal_(0.111, 0.02)
         image = self.gaussian_blur(image)
 
-        gaussian_noise = torch.randn((3, 200, 200), device='cuda')
+        '''gaussian_noise = torch.randn((3, IMAGE_SHAPE[0], IMAGE_SHAPE[1]), device='cuda')
         noise_scale = torch.rand(1, device='cuda') * 10
-        image += gaussian_noise * noise_scale
+        image += gaussian_noise * noise_scale'''
 
         # Contrast 0.6 ~ 1.4
         gamma = 0.8 * torch.rand(1, device='cuda') + 0.6
@@ -29,12 +32,12 @@ class Augmentations(nn.Module):
         image += brightness
 
         # Dropout or Heavy Gaussian Blur
-        rand = torch.rand(1, device='cuda')
+        '''rand = torch.rand(1, device='cuda')
         if rand > 0.5:
             image = self.dropout(image)
         else:
             self.heavy_blur.weight.data.normal_(0.04, 0.02)
-            image = self.heavy_blur(image)
+            image = self.heavy_blur(image)'''
 
         # Flip horizontal or vertical for NCHW
         dim = torch.rand(1, device='cuda').round().long().data.item() + 2
@@ -54,17 +57,17 @@ class Augmentations(nn.Module):
         image = F.grid_sample(image, grid)
 
         # Crop and resize
-        orig_size = (image.size(2), image.size(3))
+        '''orig_size = (image.size(2), image.size(3))
         crop_offset = torch.rand(1, device='cuda') * image.size(2) * 0.1
         crop_offset = crop_offset.long()
         crop_length = image.size(2) - crop_offset
         crop_length = crop_length.long()
         image = image[:, :, crop_offset:crop_length, crop_offset:crop_length]
-        image = F.interpolate(image, orig_size)
+        image = F.interpolate(image, orig_size)'''
 
-        # Randomly to gray
+        # Randomly to gray (25% possibility)
         rand = torch.rand(1, device='cuda')
-        if rand > 0.5:
+        if rand > 0.75:
             gray = image.sum(dim=1) / image.shape[1]
             image = torch.stack([gray, gray, gray], dim=1)
         return image
@@ -78,7 +81,7 @@ if __name__ == '__main__':
     images = []
     for file in files:
         image = cv2.imread(file)
-        image = cv2.resize(image, (200, 200))
+        image = cv2.resize(image, IMAGE_SHAPE)
         images.append(image)
 
     images = torch.Tensor(images).cuda().permute(0, 3, 1, 2).float()
