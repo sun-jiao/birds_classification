b6677a4d5fe3422bf9e15e7158e2cfe6c067daa3
Robin Dong <robin.k.dong@gmail.com>
2020-01-20 01:02:05 -0500

    Use batch instead of each sample, and limit the size of request queue

--- eval_traindata.py
+++ eval_traindata.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import numpy as np
 import os
 import queue
 import torch
@@ -7,8 +8,9 @@ import threading
 
 from efficientnet_pytorch import EfficientNet
 
+BATCH_SIZE = 32
 NR_THREADS = 3
-request_queue = queue.Queue()
+request_queue = queue.Queue(maxsize=256)
 
 
 class FileThread(threading.Thread):
@@ -24,6 +26,7 @@ class FileThread(threading.Thread):
         while (index < total):
             pair = self._file_list[index]
             img = cv2.imread(pair[0])
+            img = cv2.resize(img, (200, 200))
             request_queue.put((img, pair[1]))
             index += self._stride
         request_queue.put(("Finish", "Finish"))
@@ -36,42 +39,60 @@ class EvalThread(threading.Thread):
         self._stride = stride
         self._statistics = {}
 
+    def _process(self, img_batch, label_batch):
+        input = torch.from_numpy(np.asarray(img_batch))
+        result = self._net(input.permute(0, 3, 1, 2).float())
+        values, indices = torch.max(result, 1)
+        print(indices, label_batch)
+
+        for index in range(len(label_batch)):
+            type_id = label_batch[index]
+            predict = indices[index]
+
+            if type_id not in self._statistics:
+                self._statistics[type_id] = {}
+                self._statistics[type_id]["total"] = 0
+                self._statistics[type_id]["correct"] = 0
+
+            entry = self._statistics[type_id]
+            entry["total"] += 1
+            if predict == type_id:
+                entry["correct"] += 1
+
     def run(self):
         finish = 0
+        img_batch = []
+        label_batch = []
 
         while (True):
             obj = request_queue.get()
             img = obj[0]
-            type_name = obj[1]
+            type_id = obj[1]
             if img == "Finish":
                 finish += 1
                 if finish >= self._stride:
+                    if len(img_batch) > 0:
+                        self._process(img_batch, label_batch)
                     break
             else:
-                input = torch.from_numpy(img)
-                result = self._net(input.unsqueeze(0).permute(0, 3, 1, 2).float())
-                values, indices = torch.max(result, 1)
-                print(indices.item(), type_name)
-                if type_name not in self._statistics:
-                    self._statistics[type_name] = {}
-                    self._statistics[type_name]["total"] = 0
-                    self._statistics[type_name]["correct"] = 0
-
-                entry = self._statistics[type_name]
-                entry["total"] += 1
-                if indices.item() == type_name:
-                    entry["correct"] += 1
+                label_batch.append(type_id)
+                img_batch.append(img)
+                if len(img_batch) < BATCH_SIZE:
+                    continue
+                self._process(img_batch, label_batch)
+                img_batch = []
+                label_batch = []
 
     def dump(self):
         for type_name, values in self._statistics.items():
-            percentage = values["total"] / values["corret"]
+            percentage = values["correct"] / values["total"]
             print(type_name, values, percentage)
 
 
 def get_file_list(root_path):
     file_list = []
 
-    limit = 0
+    # limit = 0
     for directory in os.walk(root_path):
         for dir_name in directory[1]:  # All subdirectories
             pos = dir_name.find('.')
@@ -81,12 +102,12 @@ def get_file_list(root_path):
             count = 0
             for image_file in os.listdir(os.path.join(root_path, dir_name)):
                 count += 1
-            if count < 100:
+            if count < 10:
                 continue
 
-            limit += 1
-            if limit > 2:
-                break
+            # limit += 1
+            # if limit > 10:
+            #    break
 
             for image_file in os.listdir(os.path.join(root_path, dir_name)):
                 full_path = os.path.join(root_path, dir_name, image_file)
