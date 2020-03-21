63ddc034f6370167e7ac3d26bcce53dcc59f1573
Robin Dong <robin.k.dong@gmail.com>
2020-03-20 23:54:35 -0400

    Add evaluation script for chinese birds

---
+++ cn_eval.py
@@ -0,0 +1,88 @@
+import os
+import cv2
+import csv
+import time
+import argparse
+
+import torch
+
+from efficientnet_pytorch import EfficientNet
+
+IMAGE_SHAPE = (300, 300)
+
+
+def check_top5(label_map, result, real_name):
+    for id in result:
+        pred_name = label_map.get(id, None)
+        if real_name == pred_name:
+            return True
+    return False
+
+
+def load_cn_label(filename):
+    label_map = {}
+
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[1].isdigit():
+                id = int(row[1])
+                name = row[0]
+                label_map[id] = name
+
+    return label_map
+
+
+def predict(net, image_file):
+    img = cv2.imread(image_file)
+    img = cv2.resize(img, IMAGE_SHAPE)
+
+    tensor_img = torch.from_numpy(img)
+    result = net(tensor_img.unsqueeze(0).permute(0, 3, 1, 2).float())
+    values, indices = torch.topk(result, 5)
+    return indices[0].tolist()
+
+
+def main(args):
+    net = EfficientNet.from_name('efficientnet-b2', override_params={'num_classes': 11000})
+    net.load_state_dict(torch.load(args.classify_model, map_location='cpu'))
+    net.eval()
+
+    label_map = load_cn_label("V7.ioc101.id.mapping.txt")
+
+    total_top1 = 0
+    total_top5 = 0
+    total_count = 0
+    directory = next(os.walk(args.eval_data))
+    for dir_name in directory[1]:
+        top1 = 0
+        top5 = 0
+        count = 0
+        real_name = dir_name
+        for image_file in os.listdir(os.path.join(args.eval_data, dir_name)):
+            full_path = os.path.join(args.eval_data, dir_name, image_file)
+            result = predict(net, full_path)
+            # check top1
+            pred_name = label_map.get(result[0], None)
+            if pred_name == real_name:
+                top1 += 1
+                top5 += 1
+            # check top5
+            if check_top5(label_map, result[1:], real_name):
+                top5 += 1
+            count += 1
+        print(f"{real_name}, {top1/count:.4f}, {top5/count:.4f}")
+
+        total_top1 += top1
+        total_top5 += top5
+        total_count += count
+    print(f"top1 accuracy: {total_top1/total_count:.4f}")
+    print(f"top5 accuracy: {total_top5/total_count:.4f}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--eval_data', default=None, type=str)
+    parser.add_argument('--classify_model', default=None, type=str)
+    args = parser.parse_args()
+    main(args)
---
+++ cn_eval.sh
@@ -0,0 +1,2 @@
+python3 -u cn_eval.py --classify_model=ckpt/bird_cls_2720000.pth \
+  --eval_data=/media/data2/hero/birddata/V5/review/reviewset20180504/ | tee cn_eval.log
